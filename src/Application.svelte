50b3c342990167970b52d2aaaf8d4fde4e598d37
JudeAlexis111 <60825550+JudeAlexis111@users.noreply.github.com>
2023-11-26 10:47:03 -0800

    form scroller

---
+++ src/Application.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  // @ts-ignore
+  import { gsap } from "gsap";
+
+  import Blob from "./Blob.svelte";
+  import Blur from "./Blur.svelte";
+  import Background from "./Background.svelte";
+  import Frq from "./FRQ.svelte";
+
+  let scrollContainer: HTMLDivElement;
+
+  let qs = [
+    { question: "What is your name" },
+    { question: "Name what is your" },
+    { question: "Is your name what" },
+    { question: "Lorem ipsum dolor sit" },
+    { question: "Blah Bla Blah Blah" },
+  ];
+
+  function moveRight() {
+    const firstElement = qs.shift();
+    //@ts-ignore
+    qs.push(firstElement);
+
+    let mainContainerTimeline = gsap.timeline();
+
+    mainContainerTimeline.from(".frq-container", {
+      opacity: 0,
+      duration: 0.3,
+      x: -20,
+    });
+  }
+
+  function moveLeft() {
+    const lastElement = qs.pop();
+    //@ts-ignore
+    qs.unshift(lastElement);
+
+    let mainContainerTimeline = gsap.timeline();
+
+    mainContainerTimeline.from(".frq-container", {
+      opacity: 0,
+      duration: 0.3,
+      x: 20,
+    });
+  }
+
+  let lastInvocation = 0;
+  const debounceInterval = 300; // 200 ms
+
+  function debounce(func: (event: WheelEvent) => void) {
+    return function (event: WheelEvent) {
+      const now = Date.now();
+      if (now - lastInvocation > debounceInterval) {
+        func(event);
+        lastInvocation = now;
+      }
+    };
+  }
+
+  function handleScroll(event: WheelEvent) {
+    console.log(event.deltaY);
+    if (event.deltaY > 0) {
+      moveRight();
+      console.log(qs);
+    } else if (event.deltaY < 0) {
+      moveLeft();
+      console.log(qs);
+    }
+
+    qs = qs;
+  }
+
+  let debouncedHandleScroll = debounce(handleScroll);
+</script>
+
+<main on:wheel={debouncedHandleScroll}>
+  <Background />
+  <Blob />
+  <Blur />
+
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div class="applicationTitle" id="applicationTitle">Webinar Intrest Form</div>
+
+  <div bind:this={scrollContainer} class="frq-container">
+    {#each qs as q}
+      <Frq title={q.question} width={"500px"} />
+    {/each}
+  </div>
+</main>
+
+<style>
+  .frq-container {
+    display: flex;
+    width: 200%;
+    overflow-x: scroll; /* Enables horizontal scrolling */
+    white-space: nowrap; /* Prevents items from wrapping to the next line */
+    gap: 10px; /* Optional: Adds space between items */
+    padding: 10px; /* Adjust as needed */
+    opacity: 100%;
+  }
+
+  /* This ensures each Frq component has a fixed width and does not overlap */
+
+  .applicationTitle {
+    text-shadow: rgb(190, 190, 190) 1px 0 10px;
+    position: relative;
+    font-size: 54px;
+    font-weight: bold;
+    text-align: left;
+    margin: 50px;
+  }
+
+  #applicationTitle {
+    font-size: 84px;
+    font-weight: bold;
+  }
+</style>
---
+++ src/FRQ.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+  let card: HTMLDivElement;
+  export let width: string;
+  export let title: string;
+
+  //@ts-ignore
+  function cardHover(event) {
+    const { clientX, clientY } = event;
+
+    const rect = card.getBoundingClientRect(),
+      x = clientX - rect.left,
+      y = clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+</script>
+
+<main>
+  <section id="frq-container">
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+    <div
+      id="interactable"
+      style="width: {width}; max-width: {width}; min-width: {width};"
+      class="card"
+      bind:this={card}
+      on:mouseover={cardHover}
+    >
+      <div id="question-interactable">{title}</div>
+      <div id="input-holder-interactable">
+        <textarea
+          id="text-input-interactable"
+          placeholder="Type your answer here..."
+        ></textarea>
+      </div>
+    </div>
+  </section>
+</main>
+
+<style>
+  #input-holder-interactable {
+    padding: 4%;
+  }
+  #text-input-interactable {
+    width: 100%; /* Set the width to fit the container */
+    height: 45vh;
+    overflow-y: scroll;
+    background: transparent; /* Match the background with the form */
+    color: white; /* Text color */
+    font-size: 36px;
+    border: none;
+    resize: none;
+    outline: none;
+    z-index: 0;
+  }
+  #question-interactable {
+    font-size: 36px;
+    max-width: 660px;
+    text-align: left;
+  }
+  .card {
+    margin-top: 20px; /* Add some space above the text area */
+    background-color: rgba(255, 255, 255, 0.02);
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    padding: 0.5%;
+    border-radius: 10px;
+    position: relative;
+  }
+
+  .card:hover {
+    background: radial-gradient(
+      1600px circle at var(--mouse-x) var(--mouse-y),
+      rgba(255, 255, 255, 0.06),
+      transparent 40%
+    );
+    z-index: 3;
+  }
+
+  .card:hover {
+    background: radial-gradient(
+      1200px circle at var(--mouse-x) var(--mouse-y),
+      rgba(255, 255, 255, 0.15),
+      transparent 40%
+    );
+    z-index: 1;
+  }
+</style>
